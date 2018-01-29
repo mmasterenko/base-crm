37dab764ffe9ae84fabca4fad87c395fd3330980
Maksim Masterenko <mmasterenko@gmail.com>
2018-01-29 21:09:41 +0300

    :rocket: Organisation

--- apps/refbook/models/base.py
+++ apps/refbook/models/base.py
@@ -5,27 +5,104 @@ from core.models import Country, Region, City
 from project.utils.model_mixin import AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin
 
 
+PROPERTY_TYPE = (
+    (0, 'ИП'),
+    (1, 'ООО'),
+    (2, 'ОАО'),
+)
+
+CURRENCY_TYPE = (
+    (0, 'RUB'),
+)
+
+
 class PriceType(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, models.Model):
     """
     типы цен
     """
-    KIND = (
+    PRICE_KIND = (
         (0, 'закупка'),
         (1, 'продажа'),
     )
 
-    CURRENCY_TYPE = (
-        (0, 'RUB'),
-    )
-
-    kind = models.PositiveSmallIntegerField(choices=KIND, default=0)
+    kind = models.PositiveSmallIntegerField(choices=PRICE_KIND, default=0)
     title = models.CharField(max_length=64)
     currency = models.PositiveSmallIntegerField(choices=CURRENCY_TYPE, default=0)
-    note = models.CharField(max_length=512)
+    notes = models.CharField(max_length=512)
+
+
+class CashBankAccount(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, models.Model):
+    """
+    2 сущности:
+    Касса
+    Расчетный счет
+    """
+    CASHBANK_TYPE = (
+        (0, 'касса'),
+        (1, 'расчетный счет'),
+    )
+
+    cashbank_type = models.PositiveSmallIntegerField(choices=CASHBANK_TYPE)
+    title = models.CharField(max_length=128)
+    organisation = models.ForeignKey(Organisation, on_delete=models.PROTECT)
+    currency = models.PositiveSmallIntegerField(choices=CURRENCY_TYPE, default=0)
+    notes = models.CharField(max_length=512)
+    # банковские реквизиты
+    bik = models.CharField(max_length=64, blank=True, null=True, unique=True)  # БИК
+    bank_title = models.CharField(max_length=256, blank=True, null=True)  # название банка
+    korr_account = models.CharField(max_length=64, blank=True, null=True)  # корр.счёт
+    account_number = models.CharField(max_length=64, blank=True, null=True)  # номер счёта
+    account_title = models.CharField(max_length=256, blank=True, null=True)  # наименование счёта
 
 
 class Organisation(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, models.Model):
-    pass
+    """
+    организации
+    """
+    DOCUMENT_PREFIX_TYPE = ()
+
+    title = models.CharField(max_length=128)
+    property_type = models.PositiveSmallIntegerField(choices=PROPERTY_TYPE)
+    # реквизиты
+    inn = models.CharField(max_length=64, blank=True, null=True, unique=True)  # ИНН
+    kpp = models.CharField(max_length=64, blank=True, null=True, unique=True)  # КПП
+    ogrn = models.CharField(max_length=64, blank=True, null=True, unique=True)  # ОГРН
+    okpo = models.CharField(max_length=64, blank=True, null=True, unique=True)  # ОКПО
+    # данные по организации
+    phone = models.CharField(max_length=16)
+    email = models.EmailField()
+    notes = models.CharField(max_length=512)
+    # юридический адрес
+    legal_address_country = models.ForeignKey(Country, on_delete=models.PROTECT)
+    legal_address_region = models.ForeignKey(Region, on_delete=models.PROTECT)
+    legal_address_city = models.ForeignKey(City, on_delete=models.PROTECT)
+    legal_address_postcode = models.CharField(max_length=16)
+    legal_address_info = models.CharField(max_length=256)
+    # фактический адрес
+    fact_address_country = models.ForeignKey(Country, on_delete=models.PROTECT)
+    fact_address_region = models.ForeignKey(Region, on_delete=models.PROTECT)
+    fact_address_city = models.ForeignKey(City, on_delete=models.PROTECT)
+    fact_address_postcode = models.CharField(max_length=16)
+    fact_address_info = models.CharField(max_length=256)
+    # настройки учета
+    vat = models.PositiveSmallIntegerField(default=0)  # НДС (value-added tax)
+    selling_price_default_type = models.ForeignKey(PriceType, on_delete=models.PROTECT)
+    purchase_price_default_type = models.ForeignKey(PriceType, on_delete=models.PROTECT)
+    # префикс к номерам документов
+    prefix_type = None
+    prefix_string = models.CharField(max_length=16)
+    prefix_places = models.PositiveSmallIntegerField(null=True, blank=True)  # число знаков
+    # подпись и печать
+    chief = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.SET_NULL)
+    chief_position = models.CharField(max_length=128)
+    chief_signature = None  # img
+    accountant = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.SET_NULL)
+    accountant_signature = None  # img
+    organisation_seal = None  # img
+    default_bank_account = models.ForeignKey(CashBankAccount, blank=True, null=True,
+                                             on_delete=models.SET_NULL)  # расчетный счет по-умолчанию
+    default_cashbox = models.ForeignKey(CashBankAccount, blank=True, null=True,
+                                        on_delete=models.SET_NULL)  # касса по-умолчанию
 
 
 class Shop(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, models.Model):
@@ -61,41 +138,32 @@ class CounterAgent(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, 
         (2, 'женщина'),
     )
 
-    PROPERTY_TYPE = (
-        (0, 'ИП'),
-        (1, 'ООО'),
-        (2, 'ОАО'),
-    )
-
     # контрагент
     agent_type = models.PositiveSmallIntegerField(choices=AGENT_TYPE)
     title = models.CharField(max_length=256, unique=True)
     phone = models.CharField(max_length=16)
     email = models.EmailField()
     segments = models.ManyToManyField(CounterAgentSegment, related_name='counter_agents')
-
     # данные по контрагенту
     contact_name = models.CharField(max_length=128)
     sex = models.PositiveSmallIntegerField(choices=SEX, default=0)
     birth_date = models.DateField(blank=True, null=True)
     groups = models.ManyToManyField(CounterAgentGroup, related_name='counter_agents')
     responsible = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
-
-    # реквизиты
+    # тип собственности и официальное название
     property_type = models.PositiveSmallIntegerField(choices=PROPERTY_TYPE)
     formal_title = models.CharField(max_length=256)
-
+    # реквизиты
     inn = models.CharField(max_length=64, blank=True, null=True, unique=True)  # ИНН
     kpp = models.CharField(max_length=64, blank=True, null=True, unique=True)   # КПП
     ogrn = models.CharField(max_length=64, blank=True, null=True, unique=True)  # ОГРН
     okpo = models.CharField(max_length=64, blank=True, null=True, unique=True)  # ОКПО
-
+    # банковские реквизиты
     bik = models.CharField(max_length=64, blank=True, null=True, unique=True)  # БИК
     bank_title = models.CharField(max_length=256, blank=True, null=True)  # название банка
     korr_account = models.CharField(max_length=64, blank=True, null=True)  # корр.счёт
     account_number = models.CharField(max_length=64, blank=True, null=True)  # номер счёта
     account_title = models.CharField(max_length=256, blank=True, null=True)  # наименование счёта
-
     # юридический адрес
     legal_address_country = models.ForeignKey(Country, on_delete=models.PROTECT)
     legal_address_region = models.ForeignKey(Region, on_delete=models.PROTECT)
