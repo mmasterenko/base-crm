6c00e6882ac4b28a4572c8facf613c13ec1b036b
Maksim Masterenko <mmasterenko@gmail.com>
2018-02-02 18:48:12 +0300

    :rocket: store app

--- apps/store/models.py
+++ apps/store/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+from project.utils.model_mixin import AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin
+
+
+class GoodsFlow(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, models.Model):
+    """
+    перемещения товаров внутри аккаунта
+    """
+    pass
+
+
+class GoodsReceipt(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, models.Model):
+    """
+    поступления товара извне
+    """
+    pass
+
+
+class GoodsOutbound(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, models.Model):
+    """
+    отгрузка товара во вне
+    """
+    pass
+
+
+class PurchaseOrder(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, models.Model):
+    """
+    заказ поставщику
+    """
+    pass
+
+
+class RefundToCustomer(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, models.Model):
+    """
+    возвраты покупателю
+    """
+    pass
+
+
+class RefundToProvider(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, models.Model):
+    """
+    возвраты поставщику
+    """
+    pass
+
+
+class Inventory(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, models.Model):
+    """
+    инвентаризация
+    """
+    pass
+
+
+class GoodsWriteOff(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, models.Model):
+    """
+    списание товаров (по итогам инвентаризации)
+    """
+    pass
+
+
+class GoodsPlacingIn(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, models.Model):
+    """
+    оприходование товара (по итогам инвентаризации)
+    """
+    pass
--- project/settings/base.py
+++ project/settings/base.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'apps.refbook',
     'apps.crm',
     'apps.finance',
+    'apps.store',
 ]
 
 MIDDLEWARE = [
