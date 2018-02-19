96393d5c0be98c1026abe4d21d703b05cd27c788
Maksim Masterenko <mmasterenko@gmail.com>
2018-02-19 19:12:23 +0300

    :gem: IE refactoring

--- apps/finance/models.py
+++ apps/finance/models.py
@@ -14,18 +14,16 @@ class IncomeNExpense(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin
         (0, 'расход'),
         (1, 'доход'),
     )
-    is_made = models.BooleanField(default=True)  # проведен / учитывать
-    datetime = models.DateTimeField(default=timezone.now)
     ie_kind = models.PositiveSmallIntegerField(choices=IE_KIND)
+    is_made = models.BooleanField(default=True)  # проведен / учитывать
     source = models.ForeignKey(IncomeNExpenseType, on_delete=models.PROTECT)  # статья дохода/расхода
     cash_account = models.ForeignKey(CashNAccount, on_delete=models.PROTECT)
+    datetime = models.DateTimeField(default=timezone.now)
     amount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
     vat = models.PositiveSmallIntegerField()
-
     counter_agent = models.ForeignKey(CounterAgent, on_delete=models.PROTECT)
-    shop = models.ForeignKey(Shop, on_delete=models.PROTECT)
     organisation = models.ForeignKey(Organisation, on_delete=models.PROTECT)
-    responsible = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='responsible_for_inex',
+    responsible = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='responsible_for_ie',
                                     on_delete=models.PROTECT)
 
     class Meta:
@@ -41,7 +39,6 @@ class MoneyFlow(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, mod
     cash_account_sender = models.ForeignKey(CashNAccount, related_name='as_sender_flows', on_delete=models.PROTECT)
     cash_account_receiver = models.ForeignKey(CashNAccount, related_name='as_receiver_flows', on_delete=models.PROTECT)
     amount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-
     organisation = models.ForeignKey(Organisation, on_delete=models.PROTECT)
     responsible = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='responsible_for_money_flows',
                                     on_delete=models.PROTECT)
