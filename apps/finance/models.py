d47b4d54d0b0b01ca4ce2ec469dfda7eb21fe8bb
Maksim Masterenko <mmasterenko@gmail.com>
2018-02-22 18:02:57 +0300

    :gem: refactoring finance models

--- apps/finance/models.py
+++ apps/finance/models.py
@@ -8,40 +8,77 @@ from apps.refbook.models import CounterAgent, Shop, Organisation, CashNAccount, 
 
 class IncomeNExpense(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, models.Model):
     """
-    приход / расход денег извне
+    Доходы и Расходы - помещено в одну сущность, потому что они очень похожи.
+    Эта сущность отражает денежное взаимодействие аккаунта с внешним миром.
     """
     IE_KIND = (
         (0, 'расход'),
         (1, 'доход'),
     )
-    ie_kind = models.PositiveSmallIntegerField(choices=IE_KIND)
-    is_made = models.BooleanField(default=True)  # проведен / учитывать
-    source = models.ForeignKey(IncomeNExpenseType, on_delete=models.PROTECT)  # статья дохода/расхода
-    cash_account = models.ForeignKey(CashNAccount, on_delete=models.PROTECT)
-    datetime = models.DateTimeField(default=timezone.now)
-    amount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-    vat = models.PositiveSmallIntegerField()
-    counter_agent = models.ForeignKey(CounterAgent, on_delete=models.PROTECT)
-    organisation = models.ForeignKey(Organisation, on_delete=models.PROTECT)
-    responsible = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='responsible_for_ie',
+    ie_kind = models.PositiveSmallIntegerField(verbose_name='вид',
+                                               choices=IE_KIND)
+    is_made = models.BooleanField(verbose_name='проведен',
+                                  default=True)
+    source = models.ForeignKey(IncomeNExpenseType,
+                               verbose_name='статья',
+                               help_text='статья дохода/расхода',
+                               on_delete=models.PROTECT)
+    cash_account = models.ForeignKey(CashNAccount,
+                                     verbose_name='касса/расчетный счет',
+                                     on_delete=models.PROTECT)
+    datetime = models.DateTimeField(verbose_name='время операции',
+                                    default=timezone.now)
+    amount = models.DecimalField(verbose_name='сумма',
+                                 help_text='сумма перевода (в рублях)',
+                                 max_digits=10,
+                                 decimal_places=2)
+    vat = models.PositiveSmallIntegerField(verbose_name='НДС, %')
+    counter_agent = models.ForeignKey(CounterAgent,
+                                      verbose_name='контрагент',
+                                      on_delete=models.PROTECT)
+    organisation = models.ForeignKey(Organisation,
+                                     verbose_name='организация',
+                                     on_delete=models.PROTECT)
+    responsible = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                    verbose_name='ответственный',
+                                    related_name='responsible_for_ie',
                                     on_delete=models.PROTECT)
 
     class Meta:
         db_table = 'income_n_expense'
+        verbose_name = 'IncomeNExpense (Доходы/Расходы)'
 
 
 class MoneyFlow(AccountMixin, CreatorMixin, CreateUpdateMixin, ArchiveMixin, models.Model):
     """
-    перемещение денег внутри аккаунта
+    Перемещения денежных средств - эта сущность отражает
+    денежные потоки внутри аккаунта.
     """
-    is_made = models.BooleanField(default=True)  # проведен / учитывать
-    datetime = models.DateTimeField(default=timezone.now)
-    cash_account_sender = models.ForeignKey(CashNAccount, related_name='as_sender_flows', on_delete=models.PROTECT)
-    cash_account_receiver = models.ForeignKey(CashNAccount, related_name='as_receiver_flows', on_delete=models.PROTECT)
-    amount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-    organisation = models.ForeignKey(Organisation, on_delete=models.PROTECT)
-    responsible = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='responsible_for_money_flows',
+    is_made = models.BooleanField(verbose_name='проведен',
+                                  help_text='если этот флаг не установлен, тогда это перемещение нигде не учитывать',
+                                  default=True)
+    datetime = models.DateTimeField(verbose_name='время операции',
+                                    default=timezone.now)
+    cash_account_sender = models.ForeignKey(CashNAccount,
+                                            verbose_name='отправитель',
+                                            related_name='as_sender_flows',
+                                            on_delete=models.PROTECT)
+    cash_account_receiver = models.ForeignKey(CashNAccount,
+                                              verbose_name='получатель',
+                                              related_name='as_receiver_flows',
+                                              on_delete=models.PROTECT)
+    amount = models.DecimalField(verbose_name='сумма',
+                                 default=0,
+                                 max_digits=10,
+                                 decimal_places=2)
+    organisation = models.ForeignKey(Organisation,
+                                     verbose_name='организация',
+                                     on_delete=models.PROTECT)
+    responsible = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                    verbose_name='ответственный',
+                                    related_name='responsible_for_money_flows',
                                     on_delete=models.PROTECT)
 
     class Meta:
         db_table = 'money_flow'
+        verbose_name = 'MoneyFlow (Перемещение ДС)'
--- project/utils/model_mixin.py
+++ project/utils/model_mixin.py
@@ -3,29 +3,42 @@ from django.conf import settings
 
 
 class AccountMixin(models.Model):
-    account = models.ForeignKey('core.Account', on_delete=models.CASCADE)
+    account = models.ForeignKey('core.Account',
+                                verbose_name='аккаунт',
+                                help_text='аккаунт, которому принадлежит запись',
+                                on_delete=models.CASCADE)
 
     class Meta:
         abstract = True
 
 
 class CreatorMixin(models.Model):
-    creator = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                verbose_name='создатель',
+                                help_text='пользователь, который создал эту запись',
+                                null=True,
+                                on_delete=models.SET_NULL)
 
     class Meta:
         abstract = True
 
 
 class ArchiveMixin(models.Model):
-    is_archive = models.BooleanField(default=False)
+    is_archive = models.BooleanField('в архиве',
+                                     help_text='если этот флаг установлен, запись находится в архиве',
+                                     default=False)
 
     class Meta:
         abstract = True
 
 
 class CreateUpdateMixin(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField('создано',
+                                      help_text='время создания записи',
+                                      auto_now_add=True)
+    updated_at = models.DateTimeField('обновлено',
+                                      help_text='время последнего записи',
+                                      auto_now=True)
 
     class Meta:
         abstract = True
